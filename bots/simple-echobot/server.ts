738bd4f1ede4a456311abf89741a75488f2d1c09
Mieczyslaw Suska <mieczyslaw.suska@capgemini.com>
2020-02-09 23:52:18 +0100

    Added simple Echobot (Hello world :-))

---
+++ bots/simple-echobot/server.ts
@@ -0,0 +1,23 @@
+import * as resify from 'restify';
+import { BotFrameworkAdapter } from 'botbuilder';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '..', '.env') })
+const echoBotPort = process.env.EchoBotPort || 3900; 
+const server = resify.createServer();
+
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => { 
+        context.sendActivity(`Echo: ${context.activity.text}`);
+    });
+});
+
+server.listen(process.env.EchoBotPort, () => {
+    console.log(`Server is listening on port ${echoBotPort}`);
+});
